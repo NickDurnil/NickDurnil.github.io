d9e06d84bd7b20cf929b31a0e3042c2551f03eeb
NickDurnil <nickdurnil@gmail.com>
2024-07-14 16:14:33 -0400

    image preloading and rescaling

--- modules.js
+++ modules.js
@@ -19,45 +19,67 @@ $('.caption').each(function(){
         fadeAway();
     }
     });
-});
+})
 
 $('.image-parallax').each(function(){
 var img = $(this);
 var imgParent = $(this).parent();
 
-    function parallaxImage(){
-        var speed = img.data('speed');
-        var imgY = imgParent.offset().top;
-        var winY = $(this).scrollTop();
-        var winH = $(this).height();
-        var parentH = imgParent.innerHeight();
+function parallaxImage(){
+    var speed = img.data('speed');
+    var imgY = imgParent.offset().top;
+    var winY = $(this).scrollTop();
+    var winH = $(this).height();
+    var parentH = imgParent.innerHeight();
 
-        var winBottom = winY + winH;
+    var winBottom = winY + winH;
 
-        if(winBottom >= imgY && winY < imgY + parentH){
-            var imgBottom = (winBottom - imgY) * speed;
+    if(winBottom >= imgY && winY < imgY + parentH){
+        var imgBottom = (winBottom - imgY) * speed;
 
-            var imgTop = winH + parentH;
+        var imgTop = winH + parentH;
 
-            var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
-        }
-        img.css({
-            top: imgPercent + "%",
-            transform: 'translate(-50%, -' + imgPercent + '%)'
-        });
+        var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
     }
+    img.css({
+        top: imgPercent + "%",
+        transform: 'translate(-50%, -' + imgPercent + '%)'
+    });
+}
 
-    function updateHeight(){
-        var height = window.innerHeight;
-        var width = window.innerWidth;
-        if(width > height){
-            height += (width - height) / 2;
-        }
-        else{
-            height += 100;
-        }
-        img.css({
-            height: height + "px",
-        });
+function updateHeight(){
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+    if(width > height){
+        height += (width - height) / 2;
     }
+    else{
+        height += 100;
+    }
+    img.css({
+        height: height + "px",
+    })
+}
+
+$(document).on({
+    scroll: function(){
+        parallaxImage();
+    }
+});
+$(window).on({
+    load: function(){
+        parallaxImage();
+        updateHeight();
+    },
+    resize: function(){
+        if(!window.matchMedia("(pointer: coarse)").matches) {
+            updateHeight();
+        } 
+    }
+});
+
+screen.orientation.addEventListener("change", function(e) {
+    updateHeight();
+});
+
 })
